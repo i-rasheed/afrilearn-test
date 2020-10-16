f778b7f7f346741d4012e9a7aacd72b75a776e9c
Rasheed <abdulrasheedozovehe@gmail.com>
2020-10-16 18:18:17 +0100

    Updates

---
+++ precache-manifest.55d9364e279ae8591c223befd949d81d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fb7a635bd4060ab86e4e32090f5d6a",
+    "url": "/afrilearn-test/index.html"
+  },
+  {
+    "revision": "a71cda494b0e05adb45c",
+    "url": "/afrilearn-test/static/css/main.1fdec31e.chunk.css"
+  },
+  {
+    "revision": "5e88222d1cf7b0297a03",
+    "url": "/afrilearn-test/static/js/2.3c1cd78b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/afrilearn-test/static/js/2.3c1cd78b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a71cda494b0e05adb45c",
+    "url": "/afrilearn-test/static/js/main.12adb5fc.chunk.js"
+  },
+  {
+    "revision": "3abf693c520ff4d2a65a",
+    "url": "/afrilearn-test/static/js/runtime-main.f197766d.js"
+  },
+  {
+    "revision": "602e902cec87ca5c7aac66ad2c9c86ad",
+    "url": "/afrilearn-test/static/media/section1-backimg.602e902c.jpg"
+  }
+]);
